00f50e8e0c2e2388195f8713b8bce7a85de7d64f
KaedeKR <xjhrz1826@gmail.com>
2025-05-10 01:11:06 +0800

    2025.5.10

--- test.cpp
+++ test.cpp
@@ -1,3 +1,8 @@
-//
-// Created by KaedeKR on 25-5-10.
-//
+#include <cstdio>
+
+int main() {
+    constexpr int a = 10;
+    constexpr int b = 3;
+    constexpr float c = static_cast<float>(a) / static_cast<float>(b);
+    printf("%.2f\n", c);
+}
